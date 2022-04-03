461a88c97f6284ed3e27ad3afd0a0c741220e5cf
k-kolomn <93425549+k-kolomn@users.noreply.github.com>
2022-04-03 23:59:24 +0300

    Update Pediatric.java

--- src/main/java/Pediatric.java
+++ src/main/java/Pediatric.java
@@ -4,12 +4,13 @@ public class Pediatric extends Doctor implements DoctorMethod {
     String name;
     String cureEmergency;
     boolean cure;
-
+// TODO: 
     protected Pediatric(String name, String cureEmergency, boolean cure){
         super(name, cureEmergency,cure);
     }
     @Override
     public String sendOwnPrescription(String conclusion, String whichDoctor) {
         return conclusion;
+        
     }
 }
